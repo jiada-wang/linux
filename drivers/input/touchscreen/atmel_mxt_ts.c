c66c2981f3d69c35d5ab35e788602bd831ca3a56
keerthikumarp <Keerthikumar.Padmanabha@in.bosch.com>
2016-12-23 07:31:45 +0100

    input: atmel_mxt_ts: Add Missing Delay for reset handling of Atmel touch panel controller in detachable displays.
    In case of attached display, the touchpanel reset is controlled
    via imx gpio's from  atmel driver and the delay between
    touchpanel reset and the time at which the chip becomes capable to
    communicate with the host processor, has be taken care.
    
    However in case of detachable displays, the touchpanel reset is
    controlled via a deserializer gpio which is triggered just before
    the atmel driver is probed.The delay between touchpanel reset and
    the time at which the chip becomes capable to communicate (as
    specified in datasheet) was not being accounted for. This patch
    introduces that delay.
    
    Signed-off-by: keerthikumarp <Keerthikumar.Padmanabha@in.bosch.com>
    Signed-off-by: George G. Davis <george_davis@mentor.com>
    Signed-off-by: Jiada Wang <jiada_wang@mentor.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -4104,6 +4104,10 @@ static int mxt_probe(struct i2c_client *client, const struct i2c_device_id *id)
 		msleep(MXT_RESET_GPIO_TIME);
 		gpiod_set_value(data->reset_gpio, 1);
 		msleep(MXT_RESET_INVALID_CHG);
+	} else {
+		dev_dbg(&client->dev,
+			"atmel reset pin not found in device tree");
+		msleep(MXT_RESET_TIME);
 	}
 
 	error = sysfs_create_group(&client->dev.kobj, &mxt_fw_attr_group);
